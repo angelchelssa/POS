ddbfd91f6495e4d36b3df679e5e1bee8c8590af4
angelchelssa <angelchelsa150526@gmail.com>
2026-03-07 23:05:58 +0700

    retreiving anggregrates

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,20 +1,15 @@
-<h1>Data User</h1>
-
-@if(is_string($data))
-    <div style="font-weight: bold;">{{ $data }}</div>
-@else
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level</th>
-        </tr>
-        <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
-        </tr>
-    </table>
-@endif
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Data User</title>
+    </head>
+    <body>
+        <h1>Data User</h1>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <th>Jumlah Pengguna</th>
+            <tr>
+                <td>{{ $data }}</td>
+            </tr>
+        </table>
+    </body>
+</html>
