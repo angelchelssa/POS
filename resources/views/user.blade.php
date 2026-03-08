e38816afd7d90eea83393600a8278df9e97f4b10
angelchelssa <angelchelsa150526@gmail.com>
2026-03-08 19:00:46 +0700

    retreiving or creating models

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,15 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Data User</title>
-    </head>
-    <body>
-        <h1>Data User</h1>
-        <table border="1" cellpadding="2" cellspacing="0">
-            <th>Jumlah Pengguna</th>
-            <tr>
-                <td>{{ $data }}</td>
-            </tr>
-        </table>
-    </body>
-</html>
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+        </tr>
+        <tr>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
+        </tr>
+    </table>
+</body>
